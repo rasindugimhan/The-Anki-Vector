87c289457fbc1ca328a09bf53e1cace7254128a1
rasindugimhan <83225696+rasindugimhan@users.noreply.github.com>
2021-07-09 09:16:42 +0530

    Update MusicPlay.py

--- AnkiVector/modules/MusicPlay.py
+++ AnkiVector/modules/MusicPlay.py
@@ -4,11 +4,11 @@ __help__ = """
 1) Make bot admin (Group and in channel if use cplay)
 2) Start a voice chat
 3) Try /play [song name] for the first time by an admin
-4) If userbot joined enjoy music, If not add @AnkivectorMusicbot to your group and retry
+4) If userbot joined enjoy music, If not add @ShizuMusic_herobot to your group and retry
 
 * 🟡For Channel Music Play🟡 *
 
-1) Make @TheAnkiVectorMusicBot admin of your channel 
+1) Make @ShizuMusic_herobot admin of your channel 
 2) Send /userbotjoinchannel in linked group
 3) Now send commands in linked group 
 
@@ -57,14 +57,14 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 * ⚫️More tools⚫️ *
 
 • /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-• /userbotjoin: Invite @AnkivectorMusicbot Userbot to your chat 
+• /userbotjoin: Invite @ShizuMusic_herobot Userbot to your chat 
  
 * 🔷Assisten🔷 *
 
-@AnkivectorMusicbot
+@ShizuMusic_herobot
 
 * 🔸Bot🔸 *
 
-@TheAnkiVectorMusicBot
+@ShizuMusic_herobot
 """
 __mod_name__ = "Music Play"
